036b385cab3dc65e25a6feca88b8241127c291f2
Dave Paul <david.paul@showtime.net>
2015-08-11 14:44:35 -0400

    jekyll sass + livereload working

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,49 +1,76 @@
-module.exports = function(grunt) 
-{
-    require('load-grunt-tasks')(grunt); 
-    var _ =     require('underscore'),
-        log =   grunt.log.writeln
-    //, logJson =   (function(str){ log(JSON.stringify(str)); })
-    ;
-    
-    //var watch_options= {'livereload': true, 'spawn':false};
-    
-    grunt.initConfig({
-        
-        sass: {
-            options : {
-                'banner' : '/* pengbits */',
-                'sourcemap':    'none'
-            },
-            
-            update:    {
-                files :   {
-                    'css/index.css':'scss/index.scss',
-                },
 
+    // set up livereload and sass workflow with jekyll as inspired by
+    // http://thanpol.as/jekyll/jekyll-and-livereload-flow/
+    module.exports = function(grunt) 
+    {
+        require('load-grunt-tasks')(grunt); 
+        var _ =     require('underscore'),
+            log =   grunt.log.writeln
+        //, logJson =   (function(str){ log(JSON.stringify(str)); })
+        ;
+    
+        //var watch_options= {'livereload': true, 'spawn':false};
+    
+        grunt.initConfig({
+        
+            watch: {
+                stylesheets:    {
+                    files:  ['scss/**/*.scss'],
+                    tasks:  ['sass:update','copy:css']
+                },
+                templates:      {
+                    files:  ['*.html','*.md','*.yml','_posts/**','_includes/**','_layouts/**'],
+                    tasks:  ['shell:jekyll']
+                },
+                javascripts:    {
+                    files:  ['js/**/*.js']
+                },
                 options: {
-                    'style':        'expanded'
+                    'livereload': true, 'spawn':false
                 }
-            }	
-        },
-        
-        watch: {
-            stylesheets:    {
-                files:['scss/**/*.scss'],
-                tasks:['sass:update']
             },
-            templates:      {},
-            javascripts:    {},
-            options: {
-                'livereload': true, 'spawn':false
-            }
-            
-        }
         
-    });
+            sass: {
+                update: {
+                    options : {
+                        'sourcemap': 'none',
+                        'style': 'expanded',
+                    },
+                    files :   {
+                        'css/index.css':'scss/index.scss',
+                    }	
+                }
+            },
+        
+            copy: {
+                css: {
+                    files : {
+                        '_site/css/index.css':'css/index.css'
+                    }
+                }
+            },
+        
+            shell: {
+                jekyll: {
+                    command: 'rm -rf _site/*; jekyll build',
+                    stdout: true
+                }
+            },
+        
+            connect : {
+                server: {
+                    options: {
+                        port: 5000,
+                        hostname: '*',
+                        base: '_site'
+                    }
+                }
+            }
+        
+        });
     
-    // load external tasks.
-    // todo: move unorthadox imports above into this style of include:
-    // grunt.loadTasks('./Gruntisms/tasks');
-    
-};
+        grunt.registerTask('sass:refresh', ['sass:update','copy:css']);
+        grunt.registerTask('develop', ['connect:server','watch']);
+      
+    };
+

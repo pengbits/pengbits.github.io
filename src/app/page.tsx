1e9b78305b11d6c804b52e5c469b9238f269f9cb
pengbits <pengbits@gmail.com>
2025-08-08 14:19:23 -0400

    force nojekyll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 export default function Home() {
   return (
     <div>
-      <h1>Peng Bits</h1>
-      <p>Ahoy!</p>
+      <h1>Hi, I&apos;m Dave</h1>
+      <p>Nunc quis massa ac magna venenatis dignissim. Praesent porta, tortor a facilisis vehicula, leo libero iaculis nunc, eu tempor risus elit eget est. Duis fringilla vitae nisl ac interdum. </p>
     </div>
   );
 }

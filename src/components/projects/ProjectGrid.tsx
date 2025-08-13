eee58414ca0d308aae1ac89e64e2ec42178f5004
pengbits <pengbits@gmail.com>
2025-08-13 14:29:06 -0400

    responsive grid for projects

--- src/components/projects/ProjectGrid.tsx
+++ src/components/projects/ProjectGrid.tsx
@@ -4,5 +4,10 @@ import { ProjectGridItem } from "./ProjectGridItem";
 import { Grid } from "@chakra-ui/react";
 
 export const ProjectGrid = ({projects}:{projects:Project[]}) => {
-  return projects.map(p => <ProjectGridItem {...p} />)
+  return (<Grid templateColumns="repeat(2, 1fr)" gap="6">
+    {projects.map(props => <ProjectGridItem 
+      key={props.id}
+      {...props} 
+    />)}
+  </Grid>)
 }
--- src/components/projects/ProjectGridItem.tsx
+++ src/components/projects/ProjectGridItem.tsx
@@ -1,10 +1,10 @@
 import { Heading } from "@chakra-ui/react";
-import Project from "@/types/Project"
+import {Project} from "@/types/Project"
 import { GridItem } from "@chakra-ui/react";
 
 export const ProjectGridItem = ({id,title,description}:Project) => {
-  return (<div key={id}>
+  return (<GridItem  colSpan={{base:2, md:1}} style={{outline:'purple solid 1px'}}>
     <Heading size="3xl">{title}</Heading>
     <p>{description}</p>
-  </div>)
+  </GridItem>)
 }

f2d327bea0b57c874d031d3ee1bcf5158990c34b
pengbits <pengbits@gmail.com>
2025-08-16 12:25:54 -0400

    newline

--- src/lib/getProject.ts
+++ src/lib/getProject.ts
@@ -1,6 +1,7 @@
 import { ProjectDetails } from "@/types/Project"
 import DETAILS from "@/data/projects/details"
 
+
 export const getProject = ({id}:{id:string}) => {
   if(!(id in DETAILS)){
     throw new Error('no project data for'+id)

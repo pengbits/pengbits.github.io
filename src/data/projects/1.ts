2da8b19466d8b14112e77f6d2a3ba1b57ffb96bd
pengbits <pengbits@gmail.com>
2025-09-19 11:47:44 -0400

    sho.com writeup

--- src/data/projects/1.ts
+++ src/data/projects/1.ts
@@ -5,8 +5,15 @@ const meta:any = projects_map["1"]
 const data:ProjectDetails = {
   ...meta,
   body:[
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet fermentum lectus. Cras eget viverra turpis. Sed tellus risus, vulputate sed tincidunt vitae, scelerisque ut dolor. Donec feugiat enim vitae lorem laoreet tristique. Ut arcu lorem, facilisis at tempus sit amet, vestibulum in enim. Vestibulum porta, sapien eu mollis commodo, sapien dui maximus justo, vitae varius neque erat non nisi. Aliquam ultricies interdum maximus. In hac habitasse platea dictumst. Quisque ligula lectus, elementum in eros ut, malesuada eleifend ligula. Suspendisse condimentum sit amet velit nec placerat. Cras nec odio ligula. Phasellus vitae ultrices arcu. Phasellus sem erat, facilisis et varius quis, ullamcorper vel purus. Cras a felis varius, scelerisque nunc consectetur, placerat dolor. Duis nisl sem, aliquam eu sem ut, volutpat feugiat turpis.",
-    "Curabitur pulvinar, nisl varius pulvinar scelerisque, dolor ante pulvinar mauris, at rutrum urna velit vel ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce finibus metus quis erat lobortis, ac porta diam convallis. In feugiat sagittis neque, in feugiat leo cursus et. Suspendisse a lorem ac mi placerat ornare eu vitae libero. Donec pellentesque finibus aliquet. Pellentesque quis justo iaculis, convallis neque sed, scelerisque justo. Donec efficitur neque ac lectus rutrum, quis varius sapien suscipit. Fusce hendrerit nibh ac urna commodo, id faucibus libero viverra. Praesent eu elit turpis. Proin vestibulum dolor id nibh vehicula viverra. Fusce vitae bibendum erat, at aliquam dolor."
+    `Sho.com was the marketing site for Showtime, a premium cable channel, which had 4M monthly unique visitors at it's peak. 
+    
+    While not a streaming destination like Hulu or Netflix, we represented a premium channel, so the bar was quite high for the site's design, interactions and performance.
+
+    In the last iteration of the site, I pitched the use of a Design System and a reusable component library to support the build, allowing us to build features 2-3x faster than with a conventional page-based approach. This meant we could scale the site to support numerous devices/screen sizes while enforcing consistency and delivering an impactful, high-quality user experience.`
+  ,  
+   `The use of a Design System dramatically improved cross-functional collaboration with the Design and Product teams, as we gained a common language for discussing new and existing functionality, and it freed up a lot of dev and design cycles as we didn't need to reinvent the wheel every time we wanted to support a new series or implement a new feature.`
+  
+  
   ],
   video:{
     title: 'Sho.com Homepage',

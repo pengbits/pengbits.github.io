e5a94fa0159ff21690fbb56e6a78a37a58e3aefa
pengbits <pengbits@gmail.com>
2025-09-26 13:57:57 -0400

    menu for mobile

--- src/components/nav/SectionsMenu.tsx
+++ src/components/nav/SectionsMenu.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Menu, Portal } from "@chakra-ui/react"
 import { SectionsMenuIcon } from "./SectionsMenuIcon"
 import { Section } from "@/types/Section"
 type SectionsMenuProps = {
@@ -7,6 +7,23 @@ type SectionsMenuProps = {
 }
 export const SectionsMenu = ({sections, display}:SectionsMenuProps) => (
   <Box display={display}>
-    <SectionsMenuIcon />
+    <Menu.Root>
+      <Menu.Trigger asChild>
+        <SectionsMenuIcon />
+      </Menu.Trigger>
+      <Portal>
+
+        <Menu.Positioner>
+          <Menu.Content>
+          {sections.map(s => {
+            const label = s.titleShort || s.title
+            return (<Menu.Item value={label}>
+              <a href={`#${s.id}`} title={label}>{label}</a>
+            </Menu.Item>)
+          })}
+          </Menu.Content>
+        </Menu.Positioner>
+      </Portal>
+    </Menu.Root>
   </Box>
 )

c8f1263184a6a886859e17db23324f8976e596c7
pengbits <pengbits@gmail.com>
2025-09-29 15:18:19 -0400

    fixed `a` -> `Link` for production build

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -2,7 +2,7 @@ import { Box, Flex, Container, Text } from "@chakra-ui/react"
 import sections_data from "@/data/sections"
 import { SectionsMenu } from "./SectionsMenu"
 import { SectionsHorizontal } from "./SectionsHorizontal"
-
+import Link from "next/link"
 
 export const Nav = () => (
   <Box as="header" zIndex='200' position="fixed" h='18' top='0' w="100%" 
@@ -12,7 +12,9 @@ export const Nav = () => (
   >
     <Container as="nav" maxW="3xl">
       <Flex justifyContent="space-between" alignItems='center' py="4">
-        <Text flexBasis="1/2" as="h1" py="2"><a href="/">&copy; Peng Bits</a></Text>
+        <Text flexBasis="1/2" as="h1" py="2">
+          <Link href="/">&copy; Peng Bits</Link>
+        </Text>
         <SectionsHorizontal 
           display={{base:'none', md:'flex'}} sections={sections_data}>
         </SectionsHorizontal>

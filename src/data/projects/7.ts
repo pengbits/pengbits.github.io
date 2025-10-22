e31f472a54c8db383ec54929e6785cd7aa41e41b
pengbits <pengbits@gmail.com>
2025-10-21 20:46:01 -0400

    content

--- src/data/projects/7.ts
+++ src/data/projects/7.ts
@@ -8,10 +8,16 @@ const data:ProjectDetails = {
     base: "/asbo/asbo_00_512x384.png",
     md:   "/asbo/asbo_00_1024x768.png"
   }],
+  highlights: [
+    `An aggregator providing a curated feed of dj mix show content across different stations.`,
+    `React & Redux for the Client, Ruby on Rails for the server.`,
+    `Heavy influenced by principles of Test-Driven and Behavior-Driven development, using Jest and Cucumber.`,
+    `Renegade HTML scraping powered by Nokogiri!`
+  ],
   body:[
-    "Asbo was a personal project that started in the best possible circumstances: in response to a real problem that I was experiencing at the time. I have been a fan of electronic and underground club music for years, following the scenes as they mutated and spawned new genres, and I was always dialed in to what was happening in the UK. At the time, I was listening to a lot of dj mix shows, and paying close attention to several pirate/internet radio stations based in London. I noticed that a lot of the same djs that I followed would pop up on several different stations. At the same time, some of the stations would be pushing out a large nunber of shows, but I was only interested in a subset that I kept returning to week after week. One of the stations had a rss feed but most did not, and relied on soundcloud or mixcloud to host the audio.",
+    "Asbo was a personal project that started in the best possible way: in response to a real problem that I was experiencing at the time. I have been a fan of electronic and underground club music for years, following the scenes as they mutated and spawned new genres, and I was always dialed in to what was happening in the UK. At the time, I was listening to a lot of dj mix shows, and paying close attention to several pirate/internet radio stations based in London. I noticed that a lot of the same djs that I followed would pop up on several different stations. At the same time, some of the stations would be pushing out a large nunber of shows, but I was only interested in a subset that I kept returning to week after week. One of the stations had a rss feed but most did not, and relied on soundcloud or mixcloud to host the audio.",
     "I decided to build an aggregator that would abstract the different stations into a normalized 'platform' model, and make djs or 'artists' first-class citizens. I opted for Ruby on Rails for the server layer, and React & Redux for the client. I used an html scraping library (nokogiri) to coerce the output of the different station sites into a single feed, which I exposed via REST API on the server-side.",
-    "Ultimately, the project's life was short-lived. Of the three stations I was building the app around, one is now defunct, one changed to a dramatically different (and improved) ux, which invalidated my parser, and one is still standing. These days I still listen to rinse.fm almost every day, but I have to make do without a bespoke feed experience :-)."
+    "Ultimately, the project was short-lived. Of the three stations I was building the app around, one is now defunct, one changed to a dramatically different (and improved) ux, which invalidated my parser, and one is still standing. These days I still listen to rinse.fm almost every day, but I have to make do without a bespoke feed experience :-)."
   ],
 
   tags:[

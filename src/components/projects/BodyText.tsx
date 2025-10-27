d176f88e72dd8d4453beae76be7dc9be233e02fb
pengbits <pengbits@gmail.com>
2025-10-27 13:59:12 -0400

    support links, bold and italic in body of project

--- src/components/projects/BodyText.tsx
+++ src/components/projects/BodyText.tsx
@@ -1,16 +1,19 @@
 import { Text } from "@chakra-ui/react"
+import { applyFormatting } from "@/lib/markup"
 type BodyTextProps = {
-  body: string[]
+  body: string[],
+  useMarkup: boolean
 }
-export const BodyText = ({body}:BodyTextProps) => {
+export const BodyText = ({body, useMarkup}:BodyTextProps) => {
   return body.map((p,i) => {
     return <Text 
       key={i} 
       fontSize="medium" 
       as="p"
       mb="4"
-    >
-      {p}
-    </Text>
+      dangerouslySetInnerHTML={{
+        __html: applyFormatting(p)
+      }}
+    ></Text>
   })
 }

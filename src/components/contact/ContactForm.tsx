f82598a3a9c5d9e36c7d1ffbbe3b555cdb883f0d
pengbits <pengbits@gmail.com>
2025-10-28 10:01:38 -0400

    green feedback box for success

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { FormEvent, FC, useState } from 'react';
-import { Stack, Field, Input, Textarea, Button} from '@chakra-ui/react'
+import { Box, Text, Field, Input, Textarea, Button} from '@chakra-ui/react'
 import type { SendMailAttrs } from '@/types/SendMail';
 import { sendMail } from '@/lib/api';
 
@@ -43,21 +43,25 @@ export const ContactForm:FC = () => {
 
 
   return (
-  <form onSubmit={handleSubmit}>
-    {loading && <p>loading...</p>}
-    {error && <p>{error}</p>}
-    <Field.Root as="p" my="4">
-      <Field.Label htmlFor="name">Name</Field.Label>
-      <Input required size="md" name="name" type="text" placeholder="Enter your name"></Input>
-    </Field.Root>
-    <Field.Root as="p" mb="4">
-      <Field.Label htmlFor="name">Email</Field.Label>
-      <Input required size="md"  name="email" type="email" placeholder="Enter your email"></Input>
-    </Field.Root>
-    <Field.Root as="div" mb="4">
-      <Field.Label htmlFor="message">Message</Field.Label>
-      <Textarea required name="message" placeholder="Enter a message"></Textarea>
-    </Field.Root>
-    <Button as="input" type="submit" value="Send"></Button>
-  </form>)
+  <Box asChild py="4">
+    <form onSubmit={handleSubmit}>
+      {loading  && <Text as="p" mb="4">loading...</Text>}
+      {error    && <Text as="p" mb="4">{error}</Text>}
+      {success  && <Text as="p" mb="4" fontWeight="bold" borderWidth="2px" borderColor="green.600" p="2.5" color="green.600">Your Message has been sent.</Text>}
+      <Field.Root as="p" mb="4">
+        <Field.Label htmlFor="name">Name</Field.Label>
+        <Input required size="md" name="name" type="text" placeholder="Enter your name"></Input>
+      </Field.Root>
+      <Field.Root as="p" mb="4">
+        <Field.Label htmlFor="name">Email</Field.Label>
+        <Input required size="md"  name="email" type="email" placeholder="Enter your email"></Input>
+      </Field.Root>
+      <Field.Root as="div" mb="4">
+        <Field.Label htmlFor="message">Message</Field.Label>
+        <Textarea required name="message" placeholder="Enter a message"></Textarea>
+      </Field.Root>
+      <Button as="input" type="submit" value="Send"></Button>
+    </form>
+  </Box>
+  )
 }
